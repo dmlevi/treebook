7cfbf1f34233bf337c2b2124fe9ee26a88989305
Dave Levine <dmlevi@gmail.com>
2012-10-31 12:30:59 -0400

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,15 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
   has_many :statuses
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true
+                                            uniqueness: true,
+                                            format: {
+                                              with: /a-zA-Z0-9_-/,
+                                              message: 'Must be formatted correctly'
+                                            }
   
 def  full_name
 	first_name + " " + last_name	
